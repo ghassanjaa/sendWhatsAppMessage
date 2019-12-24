401d9a5943b61d50aedeb8703fd0baabc8acd95b
Ghassan Jaam <ghassan_jaam@hotmail.com>
2019-12-24 12:25:28 +0200

    Initial Commit

---
+++ SendWhatsAppMessage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SendWhatsAppMessage
+//
+//  Created by Ghassan Jaam on 12/24/19.
+//  Copyright © 2019 Ghassan Jaam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
